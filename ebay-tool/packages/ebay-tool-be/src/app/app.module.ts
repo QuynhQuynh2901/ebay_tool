1a38562dc2cd81ed03ef2509dad38c6a2ca43632
QuynhQuynh2901 <52564357+QuynhQuynh2901@users.noreply.github.com>
2022-10-31 14:43:37 +0700

    update libs

--- ebay-tool/packages/ebay-tool-be/src/app/app.module.ts
+++ ebay-tool/packages/ebay-tool-be/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common'
-import { CoreModule } from "@ebay-tool/core";
+import { CoreModule } from "@ebay-tool/core"
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
